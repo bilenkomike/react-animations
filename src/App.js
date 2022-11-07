3d669d468ecabec9ab97470705adfdd8d8225035
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-07 21:19:01 +0200

    css animations

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+
+  state = {
+      modalIsopen: false,
+  }
+
+  showModal = () => {
+    this.setState({ modalIsopen: true });
+  }
+  closeModal = () => {
+    this.setState({ modalIsopen: false });
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal show={this.state.modalIsopen} closed={this.closeModal}  />
+        <Backdrop show={this.state.modalIsopen} />
+        <button onClick={this.showModal} className="Button">Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
